11c7cc3d60ea0fce7d6f82268e40a92056dc923c
hellokiii <hellokiii@naver.com>
2017-05-22 11:41:25 +0000

    좋아요 완성

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,15 @@ class HomeController < ApplicationController
   end
   
   def booth
-    @all_jujeom = Jujeom.all
-    @popular = @all_jujeom.order(like: :desc).first(8)
     @jujeom_25 = Jujeom.where("day==25")
+    @popular = @jujeom_25.order(like: :desc).first(8)
     @unit_jujeom = Jujeom.where("day==25").select(:unit).uniq
-     end
+  end
+  
+  
   def booth2
     @jujeom_26 = Jujeom.where("day==26")
+    @popular = @jujeom_26.order(like: :desc).first(8)
     @unit_jujeom_26 = Jujeom.where("day==26").select(:unit).uniq
  
   end
@@ -27,8 +29,11 @@ class HomeController < ApplicationController
     jujeom = Jujeom.find(params[:id])
     jujeom.like += 1
     jujeom.save
-    
-    redirect_to '/home/booth'
+    if jujeom.day == "26"
+      redirect_to '/home/booth2'
+    else
+      redirect_to '/home/booth'
+    end
   end
   
   
